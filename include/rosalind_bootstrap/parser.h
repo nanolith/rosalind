3e7f2f117b23ffe7d2dbb1e7ea62bf20434f90bd
Justin Handville <nanolith@gmail.com>
2026-06-17 23:16:18 -0400

    Removed INTERFACE keyword. There's a simpler way.

--- include/rosalind_bootstrap/parser.h
+++ include/rosalind_bootstrap/parser.h
@@ -58,8 +58,6 @@ enum rcc_token_type
     RCC_TOKEN_TYPE_KEYWORD_POSTCONDITIONS,
     /** \brief INVARIANTS keyword. */
     RCC_TOKEN_TYPE_KEYWORD_INVARIANTS,
-    /** \brief INTERFACE keyword. */
-    RCC_TOKEN_TYPE_KEYWORD_INTERFACE,
 };
 
 /* C++ compatibility. */
