63ec53b204311ec64b0c7d331250b2eacb39e08b
Justin Handville <nanolith@gmail.com>
2026-06-17 23:02:47 -0400

    RCC_TOKEN_TYPE_KEYWORD_INVARIANTS decl.

--- include/rosalind_bootstrap/parser.h
+++ include/rosalind_bootstrap/parser.h
@@ -50,6 +50,8 @@ enum rcc_token_type
     RCC_TOKEN_TYPE_KEYWORD_PRECONDITIONS,
     /** \brief POSTCONDITIONS keyword. */
     RCC_TOKEN_TYPE_KEYWORD_POSTCONDITIONS,
+    /** \brief INVARIANTS keyword. */
+    RCC_TOKEN_TYPE_KEYWORD_INVARIANTS,
 };
 
 /* C++ compatibility. */
