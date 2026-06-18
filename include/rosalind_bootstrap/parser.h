99a19e53c8fc5a06a9a2235e3211875a99e5600e
Justin Handville <nanolith@gmail.com>
2026-06-17 22:35:31 -0400

    RCC_TOKEN_TYPE_KEYWORD_PRIVATE decl.

--- include/rosalind_bootstrap/parser.h
+++ include/rosalind_bootstrap/parser.h
@@ -28,6 +28,8 @@ enum rcc_token_type
     RCC_TOKEN_TYPE_KEYWORD_IMPORT,
     /** \brief PUBLIC keyword. */
     RCC_TOKEN_TYPE_KEYWORD_PUBLIC,
+    /** \brief PRIVATE keyword. */
+    RCC_TOKEN_TYPE_KEYWORD_PRIVATE,
 };
 
 /* C++ compatibility. */
