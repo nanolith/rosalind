01a89fc50aeff65a53bb5f430e1f9790e1360ee1
Justin Handville <nanolith@gmail.com>
2026-06-17 22:17:22 -0400

    Stub main for rcc-stage0.

---
+++ src/bootstrap/main.c
@@ -0,0 +1,27 @@
+/**
+ * \file fido/main.c
+ *
+ * \brief Entry point for fido.
+ *
+ * See LICENSE.txt for copyright and license information.
+ */
+
+#include <stdio.h>
+
+/**
+ * \brief Entry point for rcc.
+ *
+ * \param argc          Argument count.
+ * \param argv          Arguments.
+ *
+ * \returns 0 on success an non-zero on error.
+ */
+int main(int argc, char* argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    fprintf(stderr, "Not yet implemented.\n");
+
+    return 1;
+}
