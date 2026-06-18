7ce35fdcddaa2dee509fa6bf5a12ffcf397434f5
Justin Handville <nanolith@gmail.com>
2026-06-17 22:30:38 -0400

    RCC_TOKEN_TYPE_KEYWORD_BEGIN decl.

---
+++ include/rosalind_bootstrap/parser.h
@@ -0,0 +1,28 @@
+/**
+ * \file rosalind_bootstrap/parser.h
+ *
+ * \brief Parser and AST for rcc-stage0.
+ *
+ * See LICENSE.txt for copyright and license information.
+ */
+
+#pragma once
+
+#include <rosalind_bootstrap/error_codes.h>
+#include <rosalind_bootstrap/function_decl.h>
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+extern "C" {
+# endif /*__cplusplus*/
+
+enum rcc_token_type
+{
+    /** \brief Begin a block. */
+    RCC_TOKEN_TYPE_KEYWORD_BEGIN = 1,
+};
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+}
+# endif /*__cplusplus*/
