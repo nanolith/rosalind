2d7fa8402661cf652eb0257a63d4eb576e3e4529
Justin Handville <nanolith@gmail.com>
2026-06-17 23:07:01 -0400

    RCC_TOKEN_TYPE_KEYWORD_INTERFACE decl.

--- include/rosalind_bootstrap/parser.h
+++ include/rosalind_bootstrap/parser.h
@@ -58,6 +58,8 @@ enum rcc_token_type
     RCC_TOKEN_TYPE_KEYWORD_POSTCONDITIONS,
     /** \brief INVARIANTS keyword. */
     RCC_TOKEN_TYPE_KEYWORD_INVARIANTS,
+    /** \brief INTERFACE keyword. */
+    RCC_TOKEN_TYPE_KEYWORD_INTERFACE,
 };
 
 /* C++ compatibility. */
