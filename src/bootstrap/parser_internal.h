3b2d6b20f0b9a5e1108636c38e470e79981235bd
Justin Handville <nanolith@gmail.com>
2026-06-19 19:29:55 -0400

    parser_internal stub header.

---
+++ src/bootstrap/parser_internal.h
@@ -0,0 +1,21 @@
+/**
+ * \file bootstrap/parser_internal.h
+ *
+ * \brief Internals for rcc-stage0 parser.
+ *
+ * See LICENSE.txt for copyright and license information.
+ */
+
+#pragma once
+
+#include <rosalind_bootstrap/parser.h>
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+extern "C" {
+# endif /*__cplusplus*/
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+}
+# endif /*__cplusplus*/
