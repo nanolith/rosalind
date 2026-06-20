b66884a043f7b6ce7299472cd40d6c1235bd172c
Justin Handville <nanolith@gmail.com>
2026-06-20 12:27:56 -0400

    rcc_scanner_skip_whitespace decl.

--- src/bootstrap/parser_internal.h
+++ src/bootstrap/parser_internal.h
@@ -15,6 +15,24 @@
 extern "C" {
 # endif /*__cplusplus*/
 
+/******************************************************************************/
+/* Start of private methods.                                                  */
+/******************************************************************************/
+
+/**
+ * \brief Skip over whitespace, returning the first non-whitespace character.
+ *
+ * Update the line and column information.
+ *
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a non-whitespace character or 0.
+ *      - a non-whitespace character.
+ *      - 0 if EOF was reached.
+ */
+int FN_DECL_MUST_CHECK
+rcc_scanner_skip_whitespace(rcc_scanner* scanner);
+
 /* C++ compatibility. */
 # ifdef   __cplusplus
 }
