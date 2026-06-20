8cf570a436f0c9c9543dd94d3aefe7dd7ace0453
Justin Handville <nanolith@gmail.com>
2026-06-20 12:41:18 -0400

    rcc_scanner_next_character impl.

---
+++ src/bootstrap/rcc_scanner_next_character.c
@@ -0,0 +1,30 @@
+/**
+ * \file bootstrap/rcc_scanner_next_character.c
+ *
+ * \brief Skip to the next character.
+ *
+ * See LICENSE.txt for copyright and license information.
+ */
+
+#include "parser_internal.h"
+
+/**
+ * \brief Skip to the next character.
+ *
+ * \param scanner           The scanner instance for this operation.
+ */
+void rcc_scanner_next_character(rcc_scanner* scanner)
+{
+    if ('\n' == *(scanner->input))
+    {
+        scanner->col = 1;
+        scanner->line += 1;
+    }
+    else
+    {
+        scanner->col += 1;
+    }
+
+    scanner->input += 1;
+    scanner->index += 1;
+}
