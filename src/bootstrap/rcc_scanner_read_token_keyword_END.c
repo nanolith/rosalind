8991309d206ac8fb9e9df3219975afee1583ae1c
Justin Handville <nanolith@gmail.com>
2026-06-24 01:06:28 -0400

    rcc_scanner_read_token_keyword_END impl.

---
+++ src/bootstrap/rcc_scanner_read_token_keyword_END.c
@@ -0,0 +1,43 @@
+/**
+ * \file bootstrap/rcc_scanner_read_token_keyword_END.c
+ *
+ * \brief Read an END token.
+ *
+ * See LICENSE.txt for copyright and license information.
+ */
+
+#include <ctype.h>
+
+#include "parser_internal.h"
+
+/**
+ * \brief Attempt to read an END keyword token.
+ *
+ * \note This function exists mainly for verification purposes. The function,
+ * \ref rcc_scanner_complete_token_keyword_END is used by the scanner
+ * proper, and called by this function.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - RCC_TOKEN_TYPE_KEYWORD_END or RCC_TOKEN_TYPE_IDENTIFIER on success.
+ *      - RCC_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad input.
+ */
+int FN_DECL_MUST_CHECK
+rcc_scanner_read_token_keyword_END(
+    rcc_token_details* details, rcc_scanner* scanner)
+{
+    /* for this function, the token must begin with 'E'. */
+    if ('E' != *(scanner->input))
+    {
+        return RCC_TOKEN_TYPE_BAD_INPUT;
+    }
+
+    /* start the token. */
+    rcc_scanner_token_details_begin(details, scanner);
+
+    /* attempt to complete this token. */
+    return rcc_scanner_complete_token_keyword_END(details, scanner);
+}
