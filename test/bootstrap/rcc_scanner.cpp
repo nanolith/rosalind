bbe6b9b59249e23ce086a3c3177072802cefad84
Justin Handville <nanolith@gmail.com>
2026-06-24 00:01:43 -0400

    Test that the BEGIN keyword scanner falls back to IDENTIFIER.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -41,3 +41,32 @@ TEST(BEGIN_happy_path)
     /* clean up. */
     rcc_scanner_release(scanner);
 }
+
+/**
+ * \brief Test that BEGIN complete falls back to identifier.
+ */
+TEST(BEGIN_identifier_fallback)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "BEGINA \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* attempt to read an identifier. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_IDENTIFIER
+            == rcc_scanner_read_token_keyword_BEGIN(&details, scanner));
+
+    TEST_EXPECT(RCC_TOKEN_TYPE_IDENTIFIER == details.type);
+    TEST_EXPECT(0 == details.begin_index);
+    TEST_EXPECT(5 == details.end_index);
+    TEST_EXPECT(1 == details.begin_line);
+    TEST_EXPECT(1 == details.end_line);
+    TEST_EXPECT(1 == details.begin_col);
+    TEST_EXPECT(6 == details.end_col);
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
