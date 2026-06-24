293ec73a60c82c6a69db9a963526ea8d8c334286
Justin Handville <nanolith@gmail.com>
2026-06-24 02:40:10 -0400

    Test that the CONST complete method falls back to IDENTIFIER.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -101,6 +101,35 @@ TEST(CONST_happy_path)
 }
 
 /**
+ * \brief Test that CONST complete falls back to identifier.
+ */
+TEST(CONST_identifier_fallback)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "CONSTANT \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* attempt to read an identifier. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_IDENTIFIER
+            == rcc_scanner_read_token_keyword_CONST(&details, scanner));
+
+    TEST_EXPECT(RCC_TOKEN_TYPE_IDENTIFIER == details.type);
+    TEST_EXPECT(0 == details.begin_index);
+    TEST_EXPECT(7 == details.end_index);
+    TEST_EXPECT(1 == details.begin_line);
+    TEST_EXPECT(1 == details.end_line);
+    TEST_EXPECT(1 == details.begin_col);
+    TEST_EXPECT(8 == details.end_col);
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
+
+/**
  * \brief Test that we can read an END token.
  */
 TEST(END_happy_path)
