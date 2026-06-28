567c4d42793d11ea3567444f93f6be6cabe27ab8
Justin Handville <nanolith@gmail.com>
2026-06-28 00:24:00 -0400

    Test that we can scan a HIDE keyword.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -244,3 +244,32 @@ TEST(FUNCTION_identifier_fallback)
     /* clean up. */
     rcc_scanner_release(scanner);
 }
+
+/**
+ * \brief Test that we can read a HIDE token.
+ */
+TEST(HIDE_happy_path)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "HIDE \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* attempt to read the HIDE token. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_KEYWORD_HIDE
+            == rcc_scanner_read_token_keyword_HIDE(&details, scanner));
+
+    TEST_EXPECT(RCC_TOKEN_TYPE_KEYWORD_HIDE == details.type);
+    TEST_EXPECT(0 == details.begin_index);
+    TEST_EXPECT(3 == details.end_index);
+    TEST_EXPECT(1 == details.begin_line);
+    TEST_EXPECT(1 == details.end_line);
+    TEST_EXPECT(1 == details.begin_col);
+    TEST_EXPECT(4 == details.end_col);
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
