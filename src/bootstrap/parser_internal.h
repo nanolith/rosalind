2f84569fe877cb7b9a1890cef7c498b5d17ae4db
Justin Handville <nanolith@gmail.com>
2026-06-27 21:46:36 -0400

    rcc_scanner_complete_token_keyword_FUNCTION decl.

--- src/bootstrap/parser_internal.h
+++ src/bootstrap/parser_internal.h
@@ -108,6 +108,22 @@ rcc_scanner_complete_token_keyword_END(
     rcc_token_details* details, rcc_scanner* scanner);
 
 /**
+ * \brief Attempt to complete a FUNCTION keyword token.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - RCC_TOKEN_TYPE_KEYWORD_FUNCTION or RCC_TOKEN_TYPE_IDENTIFIER on
+ *        success.
+ *      - RCC_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad input.
+ */
+int FN_DECL_MUST_CHECK
+rcc_scanner_complete_token_keyword_FUNCTION(
+    rcc_token_details* details, rcc_scanner* scanner);
+
+/**
  * \brief Attempt to complete an identifier token.
  *
  * \param details           Pointer to the token structure to receive additional
