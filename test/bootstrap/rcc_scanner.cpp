5ff18050d7d36775ee2f27b42d8909894d6ed021
Justin Handville <nanolith@gmail.com>
2026-07-04 16:12:03 -0400

    Test that MODULE falls back to identifier.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -621,3 +621,32 @@ TEST(MODULE_happy_path)
     /* clean up. */
     rcc_scanner_release(scanner);
 }
+
+/**
+ * \brief Test that MODULE falls back to identifier.
+ */
+TEST(MODULE_identifier_fallback)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "MODULES \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* attempt to read an identifier. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_IDENTIFIER
+            == rcc_scanner_read_token_keyword_MODULE(&details, scanner));
+
+    TEST_EXPECT(RCC_TOKEN_TYPE_IDENTIFIER == details.type);
+    TEST_EXPECT(0 == details.begin_index);
+    TEST_EXPECT(6 == details.end_index);
+    TEST_EXPECT(1 == details.begin_line);
+    TEST_EXPECT(1 == details.end_line);
+    TEST_EXPECT(1 == details.begin_col);
+    TEST_EXPECT(7 == details.end_col);
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
