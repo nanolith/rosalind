63dbc582f06b9521538993d6bcc399028754fafd
Justin Handville <nanolith@gmail.com>
2026-07-04 16:29:38 -0400

    rcc_scanner_complete_token_keyword_OPAQUE decl.

--- src/bootstrap/parser_internal.h
+++ src/bootstrap/parser_internal.h
@@ -176,6 +176,22 @@ rcc_scanner_complete_token_keyword_MODULE(
     rcc_token_details* details, rcc_scanner* scanner);
 
 /**
+ * \brief Attempt to complete an OPAQUE keyword token.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - RCC_TOKEN_TYPE_KEYWORD_OPAQUE or RCC_TOKEN_TYPE_IDENTIFIER on
+ *        success.
+ *      - RCC_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad input.
+ */
+int FN_DECL_MUST_CHECK
+rcc_scanner_complete_token_keyword_OPAQUE(
+    rcc_token_details* details, rcc_scanner* scanner);
+
+/**
  * \brief Attempt to complete an identifier token.
  *
  * \param details           Pointer to the token structure to receive additional
