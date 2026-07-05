faf98db9917a872bb5471966ed929b79a828264e
Justin Handville <nanolith@gmail.com>
2026-07-04 23:26:09 -0400

    PRECONDITIONS happy path test.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -824,3 +824,32 @@ TEST(POSTCONDITIONS_identifier_fallback)
     /* clean up. */
     rcc_scanner_release(scanner);
 }
+
+/**
+ * \brief Test that we can read a PRECONDITIONS token.
+ */
+TEST(PRECONDITIONS_happy_path)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "PRECONDITIONS \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* attempt to read the PRECONDITIONS token. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_KEYWORD_PRECONDITIONS
+            == rcc_scanner_read_token_keyword_P_STAR(&details, scanner));
+
+    TEST_EXPECT(RCC_TOKEN_TYPE_KEYWORD_PRECONDITIONS == details.type);
+    TEST_EXPECT( 0 == details.begin_index);
+    TEST_EXPECT(12 == details.end_index);
+    TEST_EXPECT( 1 == details.begin_line);
+    TEST_EXPECT( 1 == details.end_line);
+    TEST_EXPECT( 1 == details.begin_col);
+    TEST_EXPECT(13 == details.end_col);
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
