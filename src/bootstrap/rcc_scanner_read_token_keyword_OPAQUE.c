940d6d9ee3f636cde94efed7791375ca1e2bdf8b
Justin Handville <nanolith@gmail.com>
2026-07-04 20:09:24 -0400

    rcc_scanner_read_token_keyword_OPAQUE impl.

---
+++ src/bootstrap/rcc_scanner_read_token_keyword_OPAQUE.c
@@ -0,0 +1,44 @@
+/**
+ * \file bootstrap/rcc_scanner_read_token_keyword_OPAQUE.c
+ *
+ * \brief Read a OPAQUE token.
+ *
+ * See LICENSE.txt for copyright and license information.
+ */
+
+#include <ctype.h>
+
+#include "parser_internal.h"
+
+/**
+ * \brief Attempt to read an OPAQUE keyword token.
+ *
+ * \note This function exists mainly for verification purposes. The function,
+ * \ref rcc_scanner_complete_token_keyword_OPAQUE is used by the scanner
+ * proper, and called by this function.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - RCC_TOKEN_TYPE_KEYWORD_OPAQUE or RCC_TOKEN_TYPE_IDENTIFIER on
+ *        success.
+ *      - RCC_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad input.
+ */
+int FN_DECL_MUST_CHECK
+rcc_scanner_read_token_keyword_OPAQUE(
+    rcc_token_details* details, rcc_scanner* scanner)
+{
+    /* for this function, the token must begin with an 'O'. */
+    if ('O' != *(scanner->input))
+    {
+        return RCC_TOKEN_TYPE_BAD_INPUT;
+    }
+
+    /* start the token. */
+    rcc_scanner_token_details_begin(details, scanner);
+
+    /* attempt to complete this token. */
+    return rcc_scanner_complete_token_keyword_OPAQUE(details, scanner);
+}
