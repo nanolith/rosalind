ffbacfe069d18603fc1588f29b75fb2e9eb2faec
Justin Handville <nanolith@gmail.com>
2026-07-05 23:42:11 -0400

    rcc_scanner_complete_token_keyword_O_STAR impl.

---
+++ src/bootstrap/rcc_scanner_complete_token_keyword_O_STAR.c
@@ -0,0 +1,171 @@
+/**
+ * \file bootstrap/rcc_scanner_complete_token_keyword_O_STAR.c
+ *
+ * \brief Complete a token beginning with O.
+ *
+ * See LICENSE.txt for copyright and license information.
+ */
+
+#include <ctype.h>
+
+#include "parser_internal.h"
+
+/* forward decls. */
+static int
+rcc_scanner_complete_token_keyword_OPAQUE_(
+    rcc_token_details* details, rcc_scanner* scanner);
+static int
+rcc_scanner_complete_token_keyword_OR(
+    rcc_token_details* details, rcc_scanner* scanner);
+
+/**
+ * \brief Attempt to complete keywords starting with an O.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - RCC_TOKEN_TYPE_KEYWORD_OPAQUE
+ *      - RCC_TOKEN_TYPE_KEYWORD_OR
+ *      - RCC_TOKEN_TYPE_IDENTIFIER
+ *      - RCC_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad input.
+ */
+int FN_DECL_MUST_CHECK
+rcc_scanner_complete_token_keyword_O_STAR(
+    rcc_token_details* details, rcc_scanner* scanner)
+{
+    int retval;
+
+    /* handle OPAQUE keyword. */
+    if ('P' == *(scanner->input + 1))
+    {
+        rcc_scanner_next_character(scanner);
+        retval = rcc_scanner_complete_token_keyword_OPAQUE_(details, scanner);
+        goto done;
+    }
+
+    /* handle OR keyword. */
+    if ('R' == *(scanner->input + 1))
+    {
+        rcc_scanner_next_character(scanner);
+        retval = rcc_scanner_complete_token_keyword_OR(details, scanner);
+        goto done;
+    }
+
+    /* identifier fallback. */
+    retval =
+        rcc_scanner_complete_token_identifier(
+            details, scanner, false);
+    goto done;
+
+done:
+    return retval;
+}
+
+/**
+ * \brief Attempt to complete an OPAQUE keyword.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - RCC_TOKEN_TYPE_KEYWORD_OPAQUE
+ *      - RCC_TOKEN_TYPE_IDENTIFIER
+ *      - RCC_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad input.
+ */
+static int
+rcc_scanner_complete_token_keyword_OPAQUE_(
+    rcc_token_details* details, rcc_scanner* scanner)
+{
+    int retval;
+
+    /* the next letter must start with A to be a keyword. */
+    if ('A' != *(scanner->input + 1))
+    {
+        goto identifier_fallback;
+    }
+    rcc_scanner_next_character(scanner);
+
+    /* the next letter must start with a Q to be a keyword. */
+    if ('Q' != *(scanner->input + 1))
+    {
+        goto identifier_fallback;
+    }
+    rcc_scanner_next_character(scanner);
+
+    /* the next letter must start with a U to be a keyword. */
+    if ('U' != *(scanner->input + 1))
+    {
+        goto identifier_fallback;
+    }
+    rcc_scanner_next_character(scanner);
+
+    /* the next letter must start with a E to be a keyword. */
+    if ('E' != *(scanner->input + 1))
+    {
+        goto identifier_fallback;
+    }
+    rcc_scanner_next_character(scanner);
+
+    /* if the token continues with an alphanumeric, this is an identifier. */
+    if (isalnum(*(scanner->input + 1)))
+    {
+        goto identifier_fallback;
+    }
+    else
+    {
+        retval =
+            rcc_scanner_token_details_end(
+                details, scanner, RCC_TOKEN_TYPE_KEYWORD_OPAQUE);
+        rcc_scanner_next_character(scanner);
+        goto done;
+    }
+
+identifier_fallback:
+    retval = rcc_scanner_complete_token_identifier(details, scanner, false);
+
+done:
+    return retval;
+}
+
+/**
+ * \brief Attempt to complete an OR keyword.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - RCC_TOKEN_TYPE_KEYWORD_OR
+ *      - RCC_TOKEN_TYPE_IDENTIFIER
+ *      - RCC_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad input.
+ */
+static int
+rcc_scanner_complete_token_keyword_OR(
+    rcc_token_details* details, rcc_scanner* scanner)
+{
+    int retval;
+
+    /* if the next letter is an alphanumeric or number, then fall back to an
+     * identifier. */
+    if (isalnum(*(scanner->input + 1)))
+    {
+        goto identifier_fallback;
+    }
+    else
+    {
+        retval =
+            rcc_scanner_token_details_end(
+                details, scanner, RCC_TOKEN_TYPE_KEYWORD_OR);
+        rcc_scanner_next_character(scanner);
+        goto done;
+    }
+
+identifier_fallback:
+    retval = rcc_scanner_complete_token_identifier(details, scanner, false);
+
+done:
+    return retval;
+}
