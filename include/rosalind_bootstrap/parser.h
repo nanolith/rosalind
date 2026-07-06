8280d34150c354a74ed81204dbe13cf6f13df1cc
Justin Handville <nanolith@gmail.com>
2026-07-06 00:01:31 -0400

    rcc_scanner_read_token_keyword_XOR decl.

--- include/rosalind_bootstrap/parser.h
+++ include/rosalind_bootstrap/parser.h
@@ -483,6 +483,26 @@ int FN_DECL_MUST_CHECK
 rcc_scanner_read_token_keyword_VAR(
     rcc_token_details* details, rcc_scanner* scanner);
 
+/**
+ * \brief Attempt to read a XOR keyword token.
+ *
+ * \note This function exists mainly for verification purposes. The function,
+ * \ref rcc_scanner_complete_token_keyword_XOR is used by the scanner
+ * proper, and called by this function.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - RCC_TOKEN_TYPE_KEYWORD_XOR or RCC_TOKEN_TYPE_IDENTIFIER on
+ *        success.
+ *      - RCC_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad input.
+ */
+int FN_DECL_MUST_CHECK
+rcc_scanner_read_token_keyword_XOR(
+    rcc_token_details* details, rcc_scanner* scanner);
+
 /* C++ compatibility. */
 # ifdef   __cplusplus
 }
