ffef867ee2fa6e1c452a282fd31558df7dff51f3
Justin Handville <nanolith@gmail.com>
2026-07-05 22:56:16 -0400

    rcc_scanner_complete_token_keyword_AND decl.

--- src/bootstrap/parser_internal.h
+++ src/bootstrap/parser_internal.h
@@ -63,6 +63,21 @@ int rcc_scanner_token_details_end(
     rcc_token_details* details, rcc_scanner* scanner, int type);
 
 /**
+ * \brief Attempt to complete an AND keyword token.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - RCC_TOKEN_TYPE_KEYWORD_AND or RCC_TOKEN_TYPE_IDENTIFIER on success.
+ *      - RCC_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad input.
+ */
+int FN_DECL_MUST_CHECK
+rcc_scanner_complete_token_keyword_AND(
+    rcc_token_details* details, rcc_scanner* scanner);
+
+/**
  * \brief Attempt to complete a BEGIN keyword token.
  *
  * \param details           Pointer to the token structure to receive additional
