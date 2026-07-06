ae4abd3e1dfd6b981949fd2848005d29131ce18a
Justin Handville <nanolith@gmail.com>
2026-07-05 23:07:16 -0400

    rcc_scanner_read_token_keyword_O_STAR decl.

--- include/rosalind_bootstrap/parser.h
+++ include/rosalind_bootstrap/parser.h
@@ -336,6 +336,27 @@ rcc_scanner_read_token_keyword_MODULE(
     rcc_token_details* details, rcc_scanner* scanner);
 
 /**
+ * \brief Attempt to read keywords starting with an O.
+ *
+ * \note This function exists mainly for verification purposes. The function,
+ * \ref rcc_scanner_complete_token_keyword_O_STAR is used by the scanner
+ * proper, and called by this function.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - RCC_TOKEN_TYPE_KEYWORD_OPAQUE
+ *      - RCC_TOKEN_TYPE_KEYWORD_OR
+ *      - RCC_TOKEN_TYPE_IDENTIFIER
+ *      - RCC_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad input.
+ */
+int FN_DECL_MUST_CHECK
+rcc_scanner_read_token_keyword_O_STAR(
+    rcc_token_details* details, rcc_scanner* scanner);
+
+/**
  * \brief Attempt to read an OPAQUE keyword token.
  *
  * \note This function exists mainly for verification purposes. The function,
