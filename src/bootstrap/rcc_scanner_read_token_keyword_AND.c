81c324da66d19d77775c529609641e0a7ee576ee
Justin Handville <nanolith@gmail.com>
2026-07-05 22:59:52 -0400

    rcc_scanner_read_token_keyword_AND impl.

---
+++ src/bootstrap/rcc_scanner_read_token_keyword_AND.c
@@ -0,0 +1,43 @@
+/**
+ * \file bootstrap/rcc_scanner_read_token_keyword_AND.c
+ *
+ * \brief Read an AND token.
+ *
+ * See LICENSE.txt for copyright and license information.
+ */
+
+#include <ctype.h>
+
+#include "parser_internal.h"
+
+/**
+ * \brief Attempt to read an AND keyword token.
+ *
+ * \note This function exists mainly for verification purposes. The function,
+ * \ref rcc_scanner_complete_token_keyword_AND is used by the scanner
+ * proper, and called by this function.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - RCC_TOKEN_TYPE_KEYWORD_AND or RCC_TOKEN_TYPE_IDENTIFIER on success.
+ *      - RCC_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad input.
+ */
+int FN_DECL_MUST_CHECK
+rcc_scanner_read_token_keyword_AND(
+    rcc_token_details* details, rcc_scanner* scanner)
+{
+    /* for this function, the token must begin with an 'A'. */
+    if ('A' != *(scanner->input))
+    {
+        return RCC_TOKEN_TYPE_BAD_INPUT;
+    }
+
+    /* start the token. */
+    rcc_scanner_token_details_begin(details, scanner);
+
+    /* attempt to complete this token. */
+    return rcc_scanner_complete_token_keyword_AND(details, scanner);
+}
