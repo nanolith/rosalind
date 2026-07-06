a642d0754f7a31825780194c89905f1b02407afe
Justin Handville <nanolith@gmail.com>
2026-07-05 21:13:12 -0400

    rcc_scanner_complete_token_keyword_VAR impl.

---
+++ src/bootstrap/rcc_scanner_complete_token_keyword_VAR.c
@@ -0,0 +1,62 @@
+/**
+ * \file bootstrap/rcc_scanner_complete_token_keyword_VAR.c
+ *
+ * \brief Complete a VAR token.
+ *
+ * See LICENSE.txt for copyright and license information.
+ */
+
+#include <ctype.h>
+
+#include "parser_internal.h"
+
+/**
+ * \brief Attempt to complete a VAR keyword token.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - RCC_TOKEN_TYPE_KEYWORD_OPAQUE or RCC_TOKEN_TYPE_IDENTIFIER on
+ *        success.
+ *      - RCC_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad input.
+ */
+int FN_DECL_MUST_CHECK
+rcc_scanner_complete_token_keyword_VAR(
+    rcc_token_details* details, rcc_scanner* scanner)
+{
+    int retval;
+
+    if ('A' != *(scanner->input + 1))
+    {
+        goto identifier_fallback;
+    }
+    rcc_scanner_next_character(scanner);
+
+    if ('R' != *(scanner->input + 1))
+    {
+        goto identifier_fallback;
+    }
+    rcc_scanner_next_character(scanner);
+
+    if (isalnum(*(scanner->input + 1)))
+    {
+        goto identifier_fallback;
+    }
+    else
+    {
+        retval =
+            rcc_scanner_token_details_end(
+                details, scanner, RCC_TOKEN_TYPE_KEYWORD_VAR);
+
+        rcc_scanner_next_character(scanner);
+
+        return retval;
+    }
+
+identifier_fallback:
+    return
+        rcc_scanner_complete_token_identifier(
+            details, scanner, false);
+}
