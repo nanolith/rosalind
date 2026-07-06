fc5419ba60e8fb466e09ec7f4048d398dd6e3e52
Justin Handville <nanolith@gmail.com>
2026-07-05 23:00:33 -0400

    Test that we can scan an AND token.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -14,6 +14,35 @@
 TEST_SUITE(rcc_scanner);
 
 /**
+ * \brief Test that we can read an AND token.
+ */
+TEST(AND_happy_path)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "AND \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* attempt to read the AND token. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_KEYWORD_AND
+            == rcc_scanner_read_token_keyword_AND(&details, scanner));
+
+    TEST_EXPECT(RCC_TOKEN_TYPE_KEYWORD_AND == details.type);
+    TEST_EXPECT(0 == details.begin_index);
+    TEST_EXPECT(2 == details.end_index);
+    TEST_EXPECT(1 == details.begin_line);
+    TEST_EXPECT(1 == details.end_line);
+    TEST_EXPECT(1 == details.begin_col);
+    TEST_EXPECT(3 == details.end_col);
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
+
+/**
  * \brief Test that we can read a BEGIN token.
  */
 TEST(BEGIN_happy_path)
