bf8515b12a6bed51b89c35e29f7b9be5e382509e
Justin Handville <nanolith@gmail.com>
2026-07-05 21:09:21 -0400

    rcc_scanner_read_token_keyword_VAR decl.

--- include/rosalind_bootstrap/parser.h
+++ include/rosalind_bootstrap/parser.h
@@ -395,6 +395,26 @@ int FN_DECL_MUST_CHECK
 rcc_scanner_read_token_keyword_T_STAR(
     rcc_token_details* details, rcc_scanner* scanner);
 
+/**
+ * \brief Attempt to read a VAR keyword token.
+ *
+ * \note This function exists mainly for verification purposes. The function,
+ * \ref rcc_scanner_complete_token_keyword_VAR is used by the scanner
+ * proper, and called by this function.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - RCC_TOKEN_TYPE_KEYWORD_OPAQUE or RCC_TOKEN_TYPE_IDENTIFIER on
+ *        success.
+ *      - RCC_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad input.
+ */
+int FN_DECL_MUST_CHECK
+rcc_scanner_read_token_keyword_VAR(
+    rcc_token_details* details, rcc_scanner* scanner);
+
 /* C++ compatibility. */
 # ifdef   __cplusplus
 }
