36bef7a20e1de1d72b0fcda8e7494efc7bd74a3a
Justin Handville <nanolith@gmail.com>
2026-07-07 15:20:04 -0400

    Test that IF scanner falls back to identifier.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -449,6 +449,35 @@ TEST(IF_happy_path)
 }
 
 /**
+ * \brief Test that IF falls back to identifier.
+ */
+TEST(IF_identifier_fallback)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "IF2 \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* attempt to read an identifier. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_IDENTIFIER
+            == rcc_scanner_read_token_keyword_I_STAR(&details, scanner));
+
+    TEST_EXPECT(RCC_TOKEN_TYPE_IDENTIFIER == details.type);
+    TEST_EXPECT(0 == details.begin_index);
+    TEST_EXPECT(2 == details.end_index);
+    TEST_EXPECT(1 == details.begin_line);
+    TEST_EXPECT(1 == details.end_line);
+    TEST_EXPECT(1 == details.begin_col);
+    TEST_EXPECT(3 == details.end_col);
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
+
+/**
  * \brief Test that we can read an IMPLEMENT token.
  */
 TEST(IMPLEMENT_happy_path)
