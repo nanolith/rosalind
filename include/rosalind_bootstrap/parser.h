a779ede33985dde457fd2c1982e2d1cc97cfa7a0
Justin Handville <nanolith@gmail.com>
2026-07-07 15:40:39 -0400

    Removed deprecated rcc_scanner_read_token_keyword_END del.

--- include/rosalind_bootstrap/parser.h
+++ include/rosalind_bootstrap/parser.h
@@ -283,25 +283,6 @@ rcc_scanner_read_token_keyword_E_STAR(
     rcc_token_details* details, rcc_scanner* scanner);
 
 /**
- * \brief Attempt to read a END keyword token.
- *
- * \note This function exists mainly for verification purposes. The function,
- * \ref rcc_scanner_complete_token_keyword_END is used by the scanner
- * proper, and called by this function.
- *
- * \param details           Pointer to the token structure to receive additional
- *                          details.
- * \param scanner           The scanner instance for this operation.
- *
- * \returns a token from the scanner.
- *      - RCC_TOKEN_TYPE_KEYWORD_END or RCC_TOKEN_TYPE_IDENTIFIER on success.
- *      - RCC_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad input.
- */
-int FN_DECL_MUST_CHECK
-rcc_scanner_read_token_keyword_END(
-    rcc_token_details* details, rcc_scanner* scanner);
-
-/**
  * \brief Attempt to read a FUNCTION keyword token.
  *
  * \note This function exists mainly for verification purposes. The function,
