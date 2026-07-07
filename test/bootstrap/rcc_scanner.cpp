767b10ad6f1ba77463f1f759cbd060af174f0788
Justin Handville <nanolith@gmail.com>
2026-07-07 13:41:11 -0400

    Test that we can scan a WHERE token.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -1580,6 +1580,35 @@ TEST(VAR_identifier_fallback)
 }
 
 /**
+ * \brief Test that we can read a WHERE token.
+ */
+TEST(WHERE_happy_path)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "WHERE \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* attempt to read the WHERE token. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_KEYWORD_WHERE
+            == rcc_scanner_read_token_keyword_W_STAR(&details, scanner));
+
+    TEST_EXPECT(RCC_TOKEN_TYPE_KEYWORD_WHERE == details.type);
+    TEST_EXPECT(0 == details.begin_index);
+    TEST_EXPECT(4 == details.end_index);
+    TEST_EXPECT(1 == details.begin_line);
+    TEST_EXPECT(1 == details.end_line);
+    TEST_EXPECT(1 == details.begin_col);
+    TEST_EXPECT(5 == details.end_col);
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
+
+/**
  * \brief Test that we can read a WHILE token.
  */
 TEST(WHILE_happy_path)
