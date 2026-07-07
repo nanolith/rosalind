029b5c99b751b1fa796e6b4754844059d2355d01
Justin Handville <nanolith@gmail.com>
2026-07-07 15:53:51 -0400

    Add support for scanning THEN keyword tokens.

--- src/bootstrap/rcc_scanner_complete_token_keyword_T_STAR.c
+++ src/bootstrap/rcc_scanner_complete_token_keyword_T_STAR.c
@@ -12,6 +12,9 @@
 
 /* forward decls. */
 static int
+rcc_scanner_complete_token_keyword_THEN(
+    rcc_token_details* details, rcc_scanner* scanner);
+static int
 rcc_scanner_complete_token_keyword_TO(
     rcc_token_details* details, rcc_scanner* scanner);
 static int
@@ -38,6 +41,14 @@ rcc_scanner_complete_token_keyword_T_STAR(
 {
     int retval;
 
+    /* handle THEN token. */
+    if ('H' == *(scanner->input + 1))
+    {
+        rcc_scanner_next_character(scanner);
+        retval = rcc_scanner_complete_token_keyword_THEN(details, scanner);
+        goto done;
+    }
+
     /* handle TO token. */
     if ('O' == *(scanner->input + 1))
     {
@@ -62,6 +73,59 @@ identifier_fallback:
         rcc_scanner_complete_token_identifier(
             details, scanner, false);
     goto done;
+
+done:
+    return retval;
+}
+
+/**
+ * \brief Attempt to complete a THEN keyword.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - RCC_TOKEN_TYPE_KEYWORD_THEN
+ *      - RCC_TOKEN_TYPE_IDENTIFIER
+ *      - RCC_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad input.
+ */
+static int
+rcc_scanner_complete_token_keyword_THEN(
+    rcc_token_details* details, rcc_scanner* scanner)
+{
+    int retval;
+
+    /* the next letter must start with E to be a keyword. */
+    if ('E' != *(scanner->input + 1))
+    {
+        goto identifier_fallback;
+    }
+    rcc_scanner_next_character(scanner);
+
+    /* the next letter must start with N to be a keyword. */
+    if ('N' != *(scanner->input + 1))
+    {
+        goto identifier_fallback;
+    }
+    rcc_scanner_next_character(scanner);
+
+    /* if the token continues with an alphanumeric, this is an identifier. */
+    if (isalnum(*(scanner->input + 1)))
+    {
+        goto identifier_fallback;
+    }
+    else
+    {
+        retval =
+            rcc_scanner_token_details_end(
+                details, scanner, RCC_TOKEN_TYPE_KEYWORD_THEN);
+        rcc_scanner_next_character(scanner);
+        goto done;
+    }
+
+identifier_fallback:
+    retval = rcc_scanner_complete_token_identifier(details, scanner, false);
 
 done:
     return retval;
