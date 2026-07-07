d650f366b062b0984e6795fb75ba7ae44c824871
Justin Handville <nanolith@gmail.com>
2026-07-07 19:36:02 -0400

    Test that we can scan a DO keyword token.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -304,6 +304,35 @@ TEST(DIV_identifier_fallback)
 }
 
 /**
+ * \brief Test that we can read a DO token.
+ */
+TEST(DO_happy_path)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "DO \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* attempt to read the DO token. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_KEYWORD_DO
+            == rcc_scanner_read_token_keyword_D_STAR(&details, scanner));
+
+    TEST_EXPECT(RCC_TOKEN_TYPE_KEYWORD_DO == details.type);
+    TEST_EXPECT(0 == details.begin_index);
+    TEST_EXPECT(1 == details.end_index);
+    TEST_EXPECT(1 == details.begin_line);
+    TEST_EXPECT(1 == details.end_line);
+    TEST_EXPECT(1 == details.begin_col);
+    TEST_EXPECT(2 == details.end_col);
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
+
+/**
  * \brief Test that we can read an ELSE token.
  */
 TEST(ELSE_happy_path)
