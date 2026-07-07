88abe175dbb325917a1ef4fd00d5b5895561e140
Justin Handville <nanolith@gmail.com>
2026-07-07 16:56:31 -0400

    Test that we can scan the OF keyword token.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -1000,6 +1000,35 @@ TEST(NOT_identifier_fallback)
 }
 
 /**
+ * \brief Test that we can read an OF token.
+ */
+TEST(OF_happy_path)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "OF \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* attempt to read the OF token. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_KEYWORD_OF
+            == rcc_scanner_read_token_keyword_O_STAR(&details, scanner));
+
+    TEST_EXPECT(RCC_TOKEN_TYPE_KEYWORD_OF == details.type);
+    TEST_EXPECT(0 == details.begin_index);
+    TEST_EXPECT(1 == details.end_index);
+    TEST_EXPECT(1 == details.begin_line);
+    TEST_EXPECT(1 == details.end_line);
+    TEST_EXPECT(1 == details.begin_col);
+    TEST_EXPECT(2 == details.end_col);
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
+
+/**
  * \brief Test that we can read an OPAQUE token.
  */
 TEST(OPAQUE_happy_path)
