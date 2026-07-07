dd9567b190a96a210ebd8463b234d8d7e3118d64
Justin Handville <nanolith@gmail.com>
2026-07-07 15:50:45 -0400

    Updated function documentation.

--- include/rosalind_bootstrap/parser.h
+++ include/rosalind_bootstrap/parser.h
@@ -488,6 +488,7 @@ rcc_scanner_read_token_keyword_R_STAR(
  * \param scanner           The scanner instance for this operation.
  *
  * \returns a token from the scanner.
+ *      - RCC_TOKEN_TYPE_KEYWORD_THEN
  *      - RCC_TOKEN_TYPE_KEYWORD_TO
  *      - RCC_TOKEN_TYPE_KEYWORD_TYPE
  *      - RCC_TOKEN_TYPE_IDENTIFIER
--- src/bootstrap/rcc_scanner_read_token_keyword_T_STAR.c
+++ src/bootstrap/rcc_scanner_read_token_keyword_T_STAR.c
@@ -22,6 +22,7 @@
  * \param scanner           The scanner instance for this operation.
  *
  * \returns a token from the scanner.
+ *      - RCC_TOKEN_TYPE_KEYWORD_THEN
  *      - RCC_TOKEN_TYPE_KEYWORD_TO
  *      - RCC_TOKEN_TYPE_KEYWORD_TYPE
  *      - RCC_TOKEN_TYPE_IDENTIFIER
