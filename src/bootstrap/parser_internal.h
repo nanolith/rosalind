61babb292aac99286ae699a95fd1525d2a30c399
Justin Handville <nanolith@gmail.com>
2026-07-07 16:15:23 -0400

    Remove unused rcc_scanner_complete_token_keyword_CONST decl.

--- src/bootstrap/parser_internal.h
+++ src/bootstrap/parser_internal.h
@@ -110,21 +110,6 @@ rcc_scanner_complete_token_keyword_C_STAR(
     rcc_token_details* details, rcc_scanner* scanner);
 
 /**
- * \brief Attempt to complete a CONST keyword token.
- *
- * \param details           Pointer to the token structure to receive additional
- *                          details.
- * \param scanner           The scanner instance for this operation.
- *
- * \returns a token from the scanner.
- *      - RCC_TOKEN_TYPE_KEYWORD_CONST or RCC_TOKEN_TYPE_IDENTIFIER on success.
- *      - RCC_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad input.
- */
-int FN_DECL_MUST_CHECK
-rcc_scanner_complete_token_keyword_CONST(
-    rcc_token_details* details, rcc_scanner* scanner);
-
-/**
  * \brief Attempt to complete a DIV keyword token.
  *
  * \param details           Pointer to the token structure to receive additional
