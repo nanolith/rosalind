95b76af179c4d5c8b8592c6db199954fff074757
Justin Handville <nanolith@gmail.com>
2026-07-07 19:43:02 -0400

    rcc_scanner_complete_token_keyword_GOTO decl.

--- src/bootstrap/parser_internal.h
+++ src/bootstrap/parser_internal.h
@@ -161,6 +161,22 @@ rcc_scanner_complete_token_keyword_F_STAR(
     rcc_token_details* details, rcc_scanner* scanner);
 
 /**
+ * \brief Attempt to complete a GOTO keyword token.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - RCC_TOKEN_TYPE_KEYWORD_GOTO or RCC_TOKEN_TYPE_IDENTIFIER on
+ *        success.
+ *      - RCC_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad input.
+ */
+int FN_DECL_MUST_CHECK
+rcc_scanner_complete_token_keyword_GOTO(
+    rcc_token_details* details, rcc_scanner* scanner);
+
+/**
  * \brief Attempt to complete a HIDE keyword token.
  *
  * \param details           Pointer to the token structure to receive additional
