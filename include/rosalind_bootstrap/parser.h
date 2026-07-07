af5ec44d45248309ef15f10d60c4e1fa767fa04a
Justin Handville <nanolith@gmail.com>
2026-07-07 15:33:16 -0400

    rcc_scanner_read_token_keyword_E_STAR decl.

--- include/rosalind_bootstrap/parser.h
+++ include/rosalind_bootstrap/parser.h
@@ -262,6 +262,27 @@ rcc_scanner_read_token_keyword_DIV(
     rcc_token_details* details, rcc_scanner* scanner);
 
 /**
+ * \brief Attempt to read keywords starting with the letter E.
+ *
+ * \note This function exists mainly for verification purposes. The function,
+ * \ref rcc_scanner_complete_token_keyword_E_STAR is used by the scanner
+ * proper, and called by this function.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - RCC_TOKEN_TYPE_KEYWORD_ELSE
+ *      - RCC_TOKEN_TYPE_KEYWORD_END
+ *      - RCC_TOKEN_TYPE_IDENTIFIER
+ *      - RCC_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad input.
+ */
+int FN_DECL_MUST_CHECK
+rcc_scanner_read_token_keyword_E_STAR(
+    rcc_token_details* details, rcc_scanner* scanner);
+
+/**
  * \brief Attempt to read a END keyword token.
  *
  * \note This function exists mainly for verification purposes. The function,
