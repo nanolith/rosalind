75902097faacdb1f70784ff26404fc55accf8565
Justin Handville <nanolith@gmail.com>
2026-07-07 19:42:28 -0400

    rcc_scanner_read_token_keyword_GOTO decl.

--- include/rosalind_bootstrap/parser.h
+++ include/rosalind_bootstrap/parser.h
@@ -320,6 +320,26 @@ rcc_scanner_read_token_keyword_F_STAR(
     rcc_token_details* details, rcc_scanner* scanner);
 
 /**
+ * \brief Attempt to read a GOTO keyword token.
+ *
+ * \note This function exists mainly for verification purposes. The function,
+ * \ref rcc_scanner_complete_token_keyword_GOTO is used by the scanner
+ * proper, and called by this function.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - RCC_TOKEN_TYPE_KEYWORD_GOTO or RCC_TOKEN_TYPE_IDENTIFIER on
+ *        success.
+ *      - RCC_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad input.
+ */
+int FN_DECL_MUST_CHECK
+rcc_scanner_read_token_keyword_GOTO(
+    rcc_token_details* details, rcc_scanner* scanner);
+
+/**
  * \brief Attempt to read a HIDE keyword token.
  *
  * \note This function exists mainly for verification purposes. The function,
