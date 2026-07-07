1157f4f6d26211449929c7d01086935c8623c03d
Justin Handville <nanolith@gmail.com>
2026-07-07 19:55:46 -0400

    Test that we can scan an IN keyword token.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -942,6 +942,35 @@ TEST(IMPORT_identifier_fallback)
 }
 
 /**
+ * \brief Test that we can read an IN token.
+ */
+TEST(IN_happy_path)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "IN \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* attempt to read the IN token. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_KEYWORD_IN
+            == rcc_scanner_read_token_keyword_I_STAR(&details, scanner));
+
+    TEST_EXPECT(RCC_TOKEN_TYPE_KEYWORD_IN == details.type);
+    TEST_EXPECT(0 == details.begin_index);
+    TEST_EXPECT(1 == details.end_index);
+    TEST_EXPECT(1 == details.begin_line);
+    TEST_EXPECT(1 == details.end_line);
+    TEST_EXPECT(1 == details.begin_col);
+    TEST_EXPECT(2 == details.end_col);
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
+
+/**
  * \brief Test that we can read an INTERFACE token.
  */
 TEST(INTERFACE_happy_path)
