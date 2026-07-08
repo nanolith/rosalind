9757fab9d2c48ab2e7ca859bd3549a9a5713139a
Justin Handville <nanolith@gmail.com>
2026-07-07 20:05:12 -0400

    rcc_scanner_complete_token_keyword_A_STAR impl.

---
+++ src/bootstrap/rcc_scanner_complete_token_keyword_A_STAR.c
@@ -0,0 +1,149 @@
+/**
+ * \file bootstrap/rcc_scanner_complete_token_keyword_A_STAR.c
+ *
+ * \brief Complete a token beginning with A.
+ *
+ * See LICENSE.txt for copyright and license information.
+ */
+
+#include <ctype.h>
+
+#include "parser_internal.h"
+
+/* forward decls. */
+static int
+rcc_scanner_complete_token_keyword_AND_(
+    rcc_token_details* details, rcc_scanner* scanner);
+static int
+rcc_scanner_complete_token_keyword_AS(
+    rcc_token_details* details, rcc_scanner* scanner);
+
+/**
+ * \brief Attempt to complete keywords starting with an A.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - RCC_TOKEN_TYPE_KEYWORD_AND
+ *      - RCC_TOKEN_TYPE_KEYWORD_AS
+ *      - RCC_TOKEN_TYPE_IDENTIFIER
+ *      - RCC_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad input.
+ */
+int FN_DECL_MUST_CHECK
+rcc_scanner_complete_token_keyword_A_STAR(
+    rcc_token_details* details, rcc_scanner* scanner)
+{
+    int retval;
+
+    /* handle AND keyword. */
+    if ('N' == *(scanner->input + 1))
+    {
+        rcc_scanner_next_character(scanner);
+        retval = rcc_scanner_complete_token_keyword_AND_(details, scanner);
+        goto done;
+    }
+
+    /* handle AS keyword. */
+    if ('S' == *(scanner->input + 1))
+    {
+        rcc_scanner_next_character(scanner);
+        retval = rcc_scanner_complete_token_keyword_AS(details, scanner);
+        goto done;
+    }
+
+    /* identifier fallback. */
+    retval =
+        rcc_scanner_complete_token_identifier(
+            details, scanner, false);
+    goto done;
+
+done:
+    return retval;
+}
+
+/**
+ * \brief Attempt to complete an AND keyword.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - RCC_TOKEN_TYPE_KEYWORD_AND
+ *      - RCC_TOKEN_TYPE_IDENTIFIER
+ *      - RCC_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad input.
+ */
+static int
+rcc_scanner_complete_token_keyword_AND_(
+    rcc_token_details* details, rcc_scanner* scanner)
+{
+    int retval;
+
+    /* the next letter must start with D to be a keyword. */
+    if ('D' != *(scanner->input + 1))
+    {
+        goto identifier_fallback;
+    }
+    rcc_scanner_next_character(scanner);
+
+    /* if the token continues with an alphanumeric, this is an identifier. */
+    if (isalnum(*(scanner->input + 1)))
+    {
+        goto identifier_fallback;
+    }
+    else
+    {
+        retval =
+            rcc_scanner_token_details_end(
+                details, scanner, RCC_TOKEN_TYPE_KEYWORD_AND);
+        rcc_scanner_next_character(scanner);
+        goto done;
+    }
+
+identifier_fallback:
+    retval = rcc_scanner_complete_token_identifier(details, scanner, false);
+
+done:
+    return retval;
+}
+
+/**
+ * \brief Attempt to complete an AS keyword.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - RCC_TOKEN_TYPE_KEYWORD_AS
+ *      - RCC_TOKEN_TYPE_IDENTIFIER
+ *      - RCC_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad input.
+ */
+static int
+rcc_scanner_complete_token_keyword_AS(
+    rcc_token_details* details, rcc_scanner* scanner)
+{
+    int retval;
+
+    /* if the token continues with an alphanumeric, this is an identifier. */
+    if (isalnum(*(scanner->input + 1)))
+    {
+        goto identifier_fallback;
+    }
+    else
+    {
+        retval =
+            rcc_scanner_token_details_end(
+                details, scanner, RCC_TOKEN_TYPE_KEYWORD_AS);
+        rcc_scanner_next_character(scanner);
+        goto done;
+    }
+
+identifier_fallback:
+    retval = rcc_scanner_complete_token_identifier(details, scanner, false);
+
+done:
+    return retval;
+}
