e799fddd3a8df7fd5409daa5af89d41092433c33
Justin Handville <nanolith@gmail.com>
2026-07-08 00:39:40 -0400

    Test that we can scan a UINT keyword token.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -2450,6 +2450,35 @@ TEST(TYPE_identifier_fallback)
 }
 
 /**
+ * \brief Test that we can read a UINT token.
+ */
+TEST(UINT_happy_path)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "UINT \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* attempt to read the UINT token. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_KEYWORD_UINT
+            == rcc_scanner_read_token_keyword_UINT(&details, scanner));
+
+    TEST_EXPECT(RCC_TOKEN_TYPE_KEYWORD_UINT == details.type);
+    TEST_EXPECT(0 == details.begin_index);
+    TEST_EXPECT(3 == details.end_index);
+    TEST_EXPECT(1 == details.begin_line);
+    TEST_EXPECT(1 == details.end_line);
+    TEST_EXPECT(1 == details.begin_col);
+    TEST_EXPECT(4 == details.end_col);
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
+
+/**
  * \brief Test that we can read a VAR token.
  */
 TEST(VAR_happy_path)
