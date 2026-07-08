b71d7686edd5202783fca77bd48edc2e2542b4c6
Justin Handville <nanolith@gmail.com>
2026-07-07 20:07:27 -0400

    Remove unused rcc_scanner_complete_token_keyword_AND decl.

--- src/bootstrap/parser_internal.h
+++ src/bootstrap/parser_internal.h
@@ -80,21 +80,6 @@ rcc_scanner_complete_token_keyword_A_STAR(
     rcc_token_details* details, rcc_scanner* scanner);
 
 /**
- * \brief Attempt to complete an AND keyword token.
- *
- * \param details           Pointer to the token structure to receive additional
- *                          details.
- * \param scanner           The scanner instance for this operation.
- *
- * \returns a token from the scanner.
- *      - RCC_TOKEN_TYPE_KEYWORD_AND or RCC_TOKEN_TYPE_IDENTIFIER on success.
- *      - RCC_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad input.
- */
-int FN_DECL_MUST_CHECK
-rcc_scanner_complete_token_keyword_AND(
-    rcc_token_details* details, rcc_scanner* scanner);
-
-/**
  * \brief Attempt to complete a BEGIN keyword token.
  *
  * \param details           Pointer to the token structure to receive additional
