ad8f8a452d5f69cfcfd4ae079930a767a5dc5090
Justin Handville <nanolith@gmail.com>
2026-07-07 22:11:28 -0400

    Test that we can scan for SET keyword tokens.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -1986,6 +1986,35 @@ TEST(REVEAL_identifier_fallback)
 }
 
 /**
+ * \brief Test that we can read a SET token.
+ */
+TEST(SET_happy_path)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "SET \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* attempt to read the SET token. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_KEYWORD_SET
+            == rcc_scanner_read_token_keyword_S_STAR(&details, scanner));
+
+    TEST_EXPECT(RCC_TOKEN_TYPE_KEYWORD_SET == details.type);
+    TEST_EXPECT(0 == details.begin_index);
+    TEST_EXPECT(2 == details.end_index);
+    TEST_EXPECT(1 == details.begin_line);
+    TEST_EXPECT(1 == details.end_line);
+    TEST_EXPECT(1 == details.begin_col);
+    TEST_EXPECT(3 == details.end_col);
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
+
+/**
  * \brief Test that we can read an SHL token.
  */
 TEST(SHL_happy_path)
