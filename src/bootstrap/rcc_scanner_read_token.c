8c7b7937e96b098d9f78b48ed775473464046a91
Justin Handville <nanolith@gmail.com>
2026-07-08 23:30:05 -0400

    rcc_scanner_read_token stub impl.

---
+++ src/bootstrap/rcc_scanner_read_token.c
@@ -0,0 +1,52 @@
+/**
+ * \file bootstrap/rcc_scanner_read_token.c
+ *
+ * \brief Read a token from the scanner instance, skipping whitespace and
+ * populating the provided token structure with details.
+ *
+ * See LICENSE.txt for copyright and license information.
+ */
+
+#include "parser_internal.h"
+
+/**
+ * \brief Read a token from the scanner instance, populating the provided token
+ * structure with details.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - RCC_SCANNER_TOKEN_TYPE_EOF at the end of input.
+ *      - RCC_SCANNER_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad
+ *        input.
+ *      - a value belonging to the \ref rcc_scanner_token_type enumeration.
+ */
+int FN_DECL_MUST_CHECK
+rcc_scanner_read_token(
+    rcc_token_details* details, rcc_scanner* scanner)
+{
+    int retval, ch;
+
+    /* read the first non-whitespace character. */
+    ch = rcc_scanner_skip_whitespace(scanner);
+    rcc_scanner_token_details_begin(details, scanner);
+
+    switch (ch)
+    {
+        case 0:
+            retval =
+                rcc_scanner_token_details_end(
+                    details, scanner, RCC_TOKEN_TYPE_EOF);
+            break;
+
+        default:
+            retval =
+                rcc_scanner_token_details_end(
+                    details, scanner, RCC_TOKEN_TYPE_BAD_INPUT);
+            break;
+    }
+
+    return retval;
+}
