fabcc789818f8f2f89476c290b5bf041d859f3bb
Justin Handville <nanolith@gmail.com>
2026-07-09 01:10:29 -0400

    Test that we can scan arbitrary identifiers.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -2854,3 +2854,32 @@ TEST(XOR_identifier_fallback)
     /* clean up. */
     rcc_scanner_release(scanner);
 }
+
+/**
+ * \brief Test that we can scan a generic identifier.
+ */
+TEST(identifier_happy_path)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "  \t foo2 \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* attempt to read an identifier. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_IDENTIFIER
+            == rcc_scanner_read_token(&details, scanner));
+
+    TEST_EXPECT(RCC_TOKEN_TYPE_IDENTIFIER == details.type);
+    TEST_EXPECT(4 == details.begin_index);
+    TEST_EXPECT(7 == details.end_index);
+    TEST_EXPECT(1 == details.begin_line);
+    TEST_EXPECT(1 == details.end_line);
+    TEST_EXPECT(5 == details.begin_col);
+    TEST_EXPECT(8 == details.end_col);
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
