557ce2e4adfddf735fe4859094b5022218f2fce0
Justin Handville <nanolith@gmail.com>
2026-07-08 23:49:10 -0400

    Added support for scanning keywords beginning with E.

--- src/bootstrap/rcc_scanner_read_token.c
+++ src/bootstrap/rcc_scanner_read_token.c
@@ -60,6 +60,11 @@ rcc_scanner_read_token(
                 rcc_scanner_complete_token_keyword_D_STAR(details, scanner);
             goto done;
 
+        case 'E':
+            retval =
+                rcc_scanner_complete_token_keyword_E_STAR(details, scanner);
+            goto done;
+
         default:
             retval =
                 rcc_scanner_token_details_end(
