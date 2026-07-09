ed7c78e3945ac5ccbe1351a3ee9d96fb3edc4b09
Justin Handville <nanolith@gmail.com>
2026-07-09 01:02:41 -0400

    Added scanner support for XOR keyword.

--- src/bootstrap/rcc_scanner_read_token.c
+++ src/bootstrap/rcc_scanner_read_token.c
@@ -130,6 +130,11 @@ rcc_scanner_read_token(
                 rcc_scanner_complete_token_keyword_VAR(details, scanner);
             goto done;
 
+        case 'X':
+            retval =
+                rcc_scanner_complete_token_keyword_XOR(details, scanner);
+            goto done;
+
         default:
             retval =
                 rcc_scanner_token_details_end(
