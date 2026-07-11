a4d163a709eb627080155e38f9d58fff9c8402dc
Justin Handville <nanolith@gmail.com>
2026-07-10 20:33:08 -0400

    Test that we can parse a negative number.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -2983,3 +2983,35 @@ TEST(too_large_number)
     /* clean up. */
     rcc_scanner_release(scanner);
 }
+
+/**
+ * \brief Test that we can scan a negative number.
+ */
+TEST(negative_number)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "  \t -123 \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* attempt to read an identifier. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_NUMBER
+            == rcc_scanner_read_token(&details, scanner));
+
+    TEST_ASSERT(RCC_TOKEN_TYPE_NUMBER == details.type);
+    TEST_EXPECT(4 == details.begin_index);
+    TEST_EXPECT(7 == details.end_index);
+    TEST_EXPECT(1 == details.begin_line);
+    TEST_EXPECT(1 == details.end_line);
+    TEST_EXPECT(5 == details.begin_col);
+    TEST_EXPECT(8 == details.end_col);
+
+    TEST_EXPECT(!details.primitive_val.integerv.is_unsigned);
+    TEST_EXPECT(-123 == details.primitive_val.integerv.val.signedv);
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
