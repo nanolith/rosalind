ea3ef4c53f7a4816c9c888ba03748cda6fadd906
Justin Handville <nanolith@gmail.com>
2026-07-12 02:15:43 -0400

    Test that OR advances the scanner.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -2534,6 +2534,37 @@ TEST(OR_identifier_fallback)
 }
 
 /**
+ * \brief Test that OR advances the scanner.
+ */
+TEST(OR_advances_scanner)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "OR OR2 \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* read an OR keyword. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_KEYWORD_OR
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read an identifier. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_IDENTIFIER
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read EOF. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_EOF
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
+
+/**
  * \brief Test that we can read a POINTER token.
  */
 TEST(POINTER_happy_path)
