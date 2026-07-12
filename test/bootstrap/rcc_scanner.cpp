7a242bd0e00c7aec4313a4749aa9d4b94876b143
Justin Handville <nanolith@gmail.com>
2026-07-12 00:44:50 -0400

    Test that FUNCTION advances the scanner.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -1021,6 +1021,37 @@ TEST(FUNCTION_identifier_fallback)
 }
 
 /**
+ * \brief Test that FUNCTION advances the scanner.
+ */
+TEST(FUNCTION_advances_scanner)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "FUNCTION FUNCTIONAL \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* read a FUNCTION keyword. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_KEYWORD_FUNCTION
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read an identifier. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_IDENTIFIER
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read EOF. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_EOF
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
+
+/**
  * \brief Test that we can read a GOTO token.
  */
 TEST(GOTO_happy_path)
