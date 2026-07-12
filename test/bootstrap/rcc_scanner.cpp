432d743f5559d9f0b477207142d6d888ce6dac36
Justin Handville <nanolith@gmail.com>
2026-07-12 02:06:28 -0400

    Test that INT advances the scanner.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -1733,6 +1733,37 @@ TEST(INT_identifier_fallback)
 }
 
 /**
+ * \brief Test that INT advances the scanner.
+ */
+TEST(INT_advances_scanner)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "INT INTO \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* read an INT keyword. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_KEYWORD_INT
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read an identifier. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_IDENTIFIER
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read EOF. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_EOF
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
+
+/**
  * \brief Test that we can read an INTERFACE token.
  */
 TEST(INTERFACE_happy_path)
