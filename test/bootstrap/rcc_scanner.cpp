46da788a3efd95fdcbd7493f3af02ee900533e92
Justin Handville <nanolith@gmail.com>
2026-07-12 02:25:00 -0400

    Test that SET advances the scanner.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -3335,6 +3335,37 @@ TEST(SET_identifier_fallback)
 }
 
 /**
+ * \brief Test that SET advances the scanner.
+ */
+TEST(SET_advances_scanner)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "SET SET2 \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* read a SET keyword. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_KEYWORD_SET
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read an identifier. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_IDENTIFIER
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read EOF. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_EOF
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
+
+/**
  * \brief Test that we can read an SHL token.
  */
 TEST(SHL_happy_path)
