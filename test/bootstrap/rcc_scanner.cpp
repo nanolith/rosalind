f84ba8ed4cbf37a45aaec9568ced7333c31fa78b
Justin Handville <nanolith@gmail.com>
2026-07-12 02:50:36 -0400

    Test that the assignment token advances the scanner.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -5363,6 +5363,37 @@ TEST(assignment_happy_path)
 }
 
 /**
+ * \brief Test that an assignment token advances the scanner.
+ */
+TEST(assignment_advances_scanner)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = ":= 123 \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* read an assignment token. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_ASSIGNMENT
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read a number token. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_NUMBER
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read EOF. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_EOF
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
+
+/**
  * \brief Test that we can scan a colon token.
  */
 TEST(colon_happy_path)
