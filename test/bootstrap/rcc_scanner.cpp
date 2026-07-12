921962b61339e23e46dfdf6724c20488bd7a3a02
Justin Handville <nanolith@gmail.com>
2026-07-12 02:37:56 -0400

    Test that number tokens advance the scanner.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -4505,6 +4505,37 @@ TEST(negative_number)
 }
 
 /**
+ * \brief Test that number advances the scanner.
+ */
+TEST(number_advances_scanner)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "123 -123 \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* read a number token. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_NUMBER
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read a number token. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_NUMBER
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read EOF. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_EOF
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
+
+/**
  * \brief Test that the negative number scanner falls back to the minus token.
  */
 TEST(negative_number_scan_minus_token_fallback)
