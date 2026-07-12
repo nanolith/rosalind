822c229c4a9582b8aa0e78ab4d91a3d1f4dbaaea
Justin Handville <nanolith@gmail.com>
2026-07-12 00:28:59 -0400

    Test that DO advances the scanner.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -665,6 +665,37 @@ TEST(DO_identifier_fallback)
 }
 
 /**
+ * \brief Test that DO advances the scanner.
+ */
+TEST(DO_advances_scanner)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "DO DONT \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* read a DO keyword. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_KEYWORD_DO
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read an identifier. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_IDENTIFIER
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read EOF. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_EOF
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
+
+/**
  * \brief Test that we can read an ELSE token.
  */
 TEST(ELSE_happy_path)
