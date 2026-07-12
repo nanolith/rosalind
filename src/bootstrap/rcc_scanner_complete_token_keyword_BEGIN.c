12412c996fdcc06df0fd326be7b02bee113ba65d
Justin Handville <nanolith@gmail.com>
2026-07-11 23:02:15 -0400

    Simplified dereferences.

--- src/bootstrap/rcc_scanner_complete_token_keyword_BEGIN.c
+++ src/bootstrap/rcc_scanner_complete_token_keyword_BEGIN.c
@@ -27,31 +27,31 @@ rcc_scanner_complete_token_keyword_BEGIN(
 {
     int retval;
 
-    if ('E' != *(scanner->input + 1))
+    if ('E' != scanner->input[1])
     {
         goto identifier_fallback;
     }
     rcc_scanner_next_character(scanner);
 
-    if ('G' != *(scanner->input + 1))
+    if ('G' != scanner->input[1])
     {
         goto identifier_fallback;
     }
     rcc_scanner_next_character(scanner);
 
-    if ('I' != *(scanner->input + 1))
+    if ('I' != scanner->input[1])
     {
         goto identifier_fallback;
     }
     rcc_scanner_next_character(scanner);
 
-    if ('N' != *(scanner->input + 1))
+    if ('N' != scanner->input[1])
     {
         goto identifier_fallback;
     }
     rcc_scanner_next_character(scanner);
 
-    if (isalnum(*(scanner->input + 1)))
+    if (isalnum(scanner->input[1]))
     {
         goto identifier_fallback;
     }
