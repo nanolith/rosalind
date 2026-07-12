9232f6da2e6d1ce4df3392657838d1340132a8bb
Justin Handville <nanolith@gmail.com>
2026-07-12 00:43:58 -0400

    Test that FOR advances the scanner.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -932,6 +932,37 @@ TEST(FOR_identifier_fallback)
 }
 
 /**
+ * \brief Test that FOR advances the scanner.
+ */
+TEST(FOR_advances_scanner)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "FOR FORTH \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* read a FOR keyword. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_KEYWORD_FOR
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read an identifier. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_IDENTIFIER
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read EOF. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_EOF
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
+
+/**
  * \brief Test that we can read a FUNCTION token.
  */
 TEST(FUNCTION_happy_path)
