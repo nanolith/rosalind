ea5ed4877407b1265f0024c71175341af9c71a16
Justin Handville <nanolith@gmail.com>
2026-07-12 02:20:15 -0400

    Test that PRIVATE advances the scanner.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -2890,6 +2890,37 @@ TEST(PRIVATE_identifier_fallback)
 }
 
 /**
+ * \brief Test that PRIVATE advances the scanner.
+ */
+TEST(PRIVATE_advances_scanner)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "PRIVATE PRIVATE2 \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* read a PRIVATE keyword. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_KEYWORD_PRIVATE
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read an identifier. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_IDENTIFIER
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read EOF. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_EOF
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
+
+/**
  * \brief Test that we can read a PROCEDURE token.
  */
 TEST(PROCEDURE_happy_path)
