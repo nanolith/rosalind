4023824fcfae14437544cb7937c99d7952f0adec
Justin Handville <nanolith@gmail.com>
2026-07-12 00:52:50 -0400

    Test that IMPLEMENTS advances the scanner.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -1466,6 +1466,37 @@ TEST(IMPLEMENTS_identifier_fallback)
 }
 
 /**
+ * \brief Test that IMPLEMENTS advances the scanner.
+ */
+TEST(IMPLEMENTS_advances_scanner)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "IMPLEMENTS IMPLEMENTS2 \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* read an IMPLEMENTS keyword. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_KEYWORD_IMPLEMENTS
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read an identifier. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_IDENTIFIER
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read EOF. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_EOF
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
+
+/**
  * \brief Test that we can read an IMPORT token.
  */
 TEST(IMPORT_happy_path)
