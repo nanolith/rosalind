c1d3e2195ec484fcdd5a4559321199fad231c308
Justin Handville <nanolith@gmail.com>
2026-07-12 02:16:50 -0400

    Test that POINTER advances the scanner.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -2623,6 +2623,37 @@ TEST(POINTER_identifier_fallback)
 }
 
 /**
+ * \brief Test that POINTER advances the scanner.
+ */
+TEST(POINTER_advances_scanner)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "POINTER POINTERS \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* read a POINTER keyword. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_KEYWORD_POINTER
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read an identifier. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_IDENTIFIER
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read EOF. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_EOF
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
+
+/**
  * \brief Test that we can read a POSTCONDITIONS token.
  */
 TEST(POSTCONDITIONS_happy_path)
