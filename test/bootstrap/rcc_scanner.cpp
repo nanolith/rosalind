df64c0e94078288445baa30a795190f835265bf3
Justin Handville <nanolith@gmail.com>
2026-07-12 02:32:27 -0400

    Test that VAR advances the scanner.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -4047,6 +4047,37 @@ TEST(VAR_identifier_fallback)
 }
 
 /**
+ * \brief Test that VAR advances the scanner.
+ */
+TEST(VAR_advances_scanner)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "VAR VARS \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* read a VAR keyword. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_KEYWORD_VAR
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read an identifier. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_IDENTIFIER
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read EOF. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_EOF
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
+
+/**
  * \brief Test that we can read a WHERE token.
  */
 TEST(WHERE_happy_path)
