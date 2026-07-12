969c184f1bcb372f11aabadfaf0648c0a284c114
Justin Handville <nanolith@gmail.com>
2026-07-12 02:28:28 -0400

    Test that THEN advances the scanner.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -3691,6 +3691,37 @@ TEST(THEN_identifier_fallback)
 }
 
 /**
+ * \brief Test that THEN advances the scanner.
+ */
+TEST(THEN_advances_scanner)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "THEN THEN2 \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* read a THEN keyword. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_KEYWORD_THEN
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read an identifier. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_IDENTIFIER
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read EOF. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_EOF
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
+
+/**
  * \brief Test that we can read a TO token.
  */
 TEST(TO_happy_path)
