b123c6ca545879efcf0d9b2b0b8dc96704f31719
Justin Handville <nanolith@gmail.com>
2026-07-11 22:47:08 -0400

    Test that we can scan not-equal tokens.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -3416,3 +3416,32 @@ TEST(less_than_happy_path)
     /* clean up. */
     rcc_scanner_release(scanner);
 }
+
+/**
+ * \brief Test that we can scan a not-equal token.
+ */
+TEST(not_equal_happy_path)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "  \t <> \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* attempt to read a less-than-equal. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_NOT_EQUAL
+            == rcc_scanner_read_token(&details, scanner));
+
+    TEST_ASSERT(RCC_TOKEN_TYPE_NOT_EQUAL == details.type);
+    TEST_EXPECT(4 == details.begin_index);
+    TEST_EXPECT(5 == details.end_index);
+    TEST_EXPECT(1 == details.begin_line);
+    TEST_EXPECT(1 == details.end_line);
+    TEST_EXPECT(5 == details.begin_col);
+    TEST_EXPECT(6 == details.end_col);
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
