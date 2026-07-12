dc039f8811081a0ae52ded41eedbf3314dba8f20
Justin Handville <nanolith@gmail.com>
2026-07-12 00:28:05 -0400

    Test that DIV advances the scanner.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -576,6 +576,37 @@ TEST(DIV_identifier_fallback)
 }
 
 /**
+ * \brief Test that DIV advances the scanner.
+ */
+TEST(DIV_advances_scanner)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "DIV DIVIDE \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* read a DIV keyword. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_KEYWORD_DIV
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read an identifier. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_IDENTIFIER
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read EOF. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_EOF
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
+
+/**
  * \brief Test that we can read a DO token.
  */
 TEST(DO_happy_path)
