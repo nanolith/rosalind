b2c57172c48201962b6b58230b44618ca04ac7cb
Justin Handville <nanolith@gmail.com>
2026-07-11 21:37:15 -0400

    Scan plus tokens.

--- src/bootstrap/rcc_scanner_read_token.c
+++ src/bootstrap/rcc_scanner_read_token.c
@@ -58,6 +58,12 @@ rcc_scanner_read_token(
                 rcc_scanner_complete_token_number(details, scanner, 8);
             goto done;
 
+        case '+':
+            retval =
+                rcc_scanner_token_details_end(
+                    details, scanner, RCC_TOKEN_TYPE_PLUS);
+            goto done;
+
         case '-':
             retval =
                 rcc_scanner_complete_token_number(details, scanner, 10);
