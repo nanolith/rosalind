f193fdb05290b850115ed8558d913b479b708a2d
Justin Handville <nanolith@gmail.com>
2026-07-12 02:44:16 -0400

    Test that the less-than-equal token advances the scanner.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -5063,6 +5063,37 @@ TEST(less_than_equal_happy_path)
 }
 
 /**
+ * \brief Test that a less-than-equal token advances the scanner.
+ */
+TEST(less_than_equal_advances_scanner)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "<= 123 \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* read a less-than-equal token. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_LESS_THAN_EQUAL
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read a number token. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_NUMBER
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read EOF. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_EOF
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
+
+/**
  * \brief Test that we can scan a less-than token.
  */
 TEST(less_than_happy_path)
