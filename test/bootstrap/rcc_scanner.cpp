28270cdb4fd2add873e6c76c368cdbbb6eff5645
Justin Handville <nanolith@gmail.com>
2026-07-12 02:12:15 -0400

    Test that NIL advances the scanner.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -2178,6 +2178,37 @@ TEST(NIL_identifier_fallback)
 }
 
 /**
+ * \brief Test that NIL advances the scanner.
+ */
+TEST(NIL_advances_scanner)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "NIL NILS \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* read a NIL keyword. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_KEYWORD_NIL
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read an identifier. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_IDENTIFIER
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read EOF. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_EOF
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
+
+/**
  * \brief Test that we can read a NOT token.
  */
 TEST(NOT_happy_path)
