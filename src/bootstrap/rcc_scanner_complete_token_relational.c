0b423d990ceca4e0e53539a7ddbd78f4975d411a
Justin Handville <nanolith@gmail.com>
2026-07-11 22:41:33 -0400

    rcc_scanner_complete_token_relational partial impl.

---
+++ src/bootstrap/rcc_scanner_complete_token_relational.c
@@ -0,0 +1,62 @@
+/**
+ * \file bootstrap/rcc_scanner_complete_token_relational.c
+ *
+ * \brief Complete a relational token, or downgrade to a simple token.
+ *
+ * See LICENSE.txt for copyright and license information.
+ */
+
+#include <ctype.h>
+#include <errno.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "parser_internal.h"
+
+/**
+ * \brief Attempt to complete a relational token.
+ *
+ * \param details           Pointer to the token structure to receive additional
+ *                          details.
+ * \param scanner           The scanner instance for this operation.
+ *
+ * \returns a token from the scanner.
+ *      - RCC_TOKEN_TYPE_ASSIGNMENT
+ *      - RCC_TOKEN_TYPE_COLON
+ *      - RCC_TOKEN_TYPE_GREATER_THAN
+ *      - RCC_TOKEN_TYPE_GREATER_THAN_EQUAL
+ *      - RCC_TOKEN_TYPE_LESS_THAN
+ *      - RCC_TOKEN_TYPE_LESS_THAN_EQUAL
+ *      - RCC_TOKEN_TYPE_NOT_EQUAL
+ *      - RCC_TOKEN_TYPE_BAD_INPUT if the scanner encounters bad input.
+ */
+int FN_DECL_MUST_CHECK
+rcc_scanner_complete_token_relational(
+    rcc_token_details* details, rcc_scanner* scanner)
+{
+    int retval;
+
+    /* Is the next character an equal sign? */
+    if ('=' == scanner->input[1])
+    {
+        switch (scanner->input[0])
+        {
+            case '<':
+                rcc_scanner_next_character(scanner);
+                retval =
+                    rcc_scanner_token_details_end(
+                        details, scanner, RCC_TOKEN_TYPE_LESS_THAN_EQUAL);
+                goto done;
+        }
+    }
+
+    /* if nothing else matches, this is bad input. */
+    retval =
+        rcc_scanner_token_details_end(
+            details, scanner, RCC_TOKEN_TYPE_BAD_INPUT);
+    goto done;
+
+done:
+    return retval;
+}
