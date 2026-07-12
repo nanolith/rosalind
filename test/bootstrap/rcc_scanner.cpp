7b83ac7aa83095ef1becb49424172d1bf36d8b2c
Justin Handville <nanolith@gmail.com>
2026-07-12 00:23:39 -0400

    Test that CASE advances the scanner.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -398,6 +398,37 @@ TEST(CASE_identifier_fallback)
 }
 
 /**
+ * \brief Test that CASE advances the scanner.
+ */
+TEST(CASE_advances_scanner)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "CASE CASE2 \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* read a CASE keyword. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_KEYWORD_CASE
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read an identifier. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_IDENTIFIER
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read EOF. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_EOF
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
+
+/**
  * \brief Test that we can read a CONST token.
  */
 TEST(CONST_happy_path)
