06af5c48c6b0f2c03c1b58d6aca2d270764980b2
Justin Handville <nanolith@gmail.com>
2026-07-12 02:05:33 -0400

    Test that IN advances the scanner.

--- test/bootstrap/rcc_scanner.cpp
+++ test/bootstrap/rcc_scanner.cpp
@@ -1644,6 +1644,37 @@ TEST(IN_identifier_fallback)
 }
 
 /**
+ * \brief Test that IN advances the scanner.
+ */
+TEST(IN_advances_scanner)
+{
+    rcc_scanner* scanner = nullptr;
+    rcc_token_details details;
+    const char* INPUT = "IN IN2 \t ";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(0 == rcc_scanner_create(&scanner, INPUT));
+
+    /* read an IN keyword. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_KEYWORD_IN
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read an identifier. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_IDENTIFIER
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* read EOF. */
+    TEST_ASSERT(
+        RCC_TOKEN_TYPE_EOF
+            == rcc_scanner_read_token(&details, scanner));
+
+    /* clean up. */
+    rcc_scanner_release(scanner);
+}
+
+/**
  * \brief Test that we can read an INT token.
  */
 TEST(INT_happy_path)
