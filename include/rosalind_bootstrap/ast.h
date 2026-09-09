2444ae52199a383c5c12cf049a0baceaab9e00f1
Justin Handville <nanolith@gmail.com>
2026-09-09 04:47:50 -0400

    Added module location token details for diagnostics.

--- include/rosalind_bootstrap/ast.h
+++ include/rosalind_bootstrap/ast.h
@@ -9,6 +9,7 @@
 #pragma once
 
 #include <rosalind_bootstrap/function_decl.h>
+#include <rosalind_bootstrap/token.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -25,6 +26,7 @@ typedef struct rcc_ast_module rcc_ast_module;
 struct rcc_ast_module
 {
     char* name;
+    rcc_token_details module_loc;
 };
 
 /* C++ compatibility. */
