b73f2dd8e77e243b1be202cee3ad90e33fee7495
Justin Handville <nanolith@gmail.com>
2026-09-09 04:43:45 -0400

    rcc_ast_module decl.

---
+++ include/rosalind_bootstrap/ast.h
@@ -0,0 +1,33 @@
+/**
+ * \file rosalind_bootstrap/ast.h
+ *
+ * \brief AST for rcc-stage0.
+ *
+ * See LICENSE.txt for copyright and license information.
+ */
+
+#pragma once
+
+#include <rosalind_bootstrap/function_decl.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+extern "C" {
+# endif /*__cplusplus*/
+
+/**
+ * \brief Module definition.
+ */
+typedef struct rcc_ast_module rcc_ast_module;
+struct rcc_ast_module
+{
+    char* name;
+};
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+}
+# endif /*__cplusplus*/
