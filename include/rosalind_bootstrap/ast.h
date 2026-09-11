78fde91569d964d59dcfe88a638c150cb57eb999
Justin Handville <nanolith@gmail.com>
2026-09-11 02:01:09 -0400

    next member decl.

--- include/rosalind_bootstrap/ast.h
+++ include/rosalind_bootstrap/ast.h
@@ -36,6 +36,7 @@ struct rcc_ast_import_spec_atom
 typedef struct rcc_ast_import rcc_ast_import;
 struct rcc_ast_import
 {
+    rcc_ast_import* next;
     char* name;
     char* shorthand;
     rcc_token_details loc;
